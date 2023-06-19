dfc91d788e0e82434b8efab5ad20fe40969579a8
prushton2 <peterrushton418@gmail.com>
2023-06-18 21:08:57 -0400

    Uploading all the stuff

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SingleFileServer
 A backend server that uploads and stores a single text file.
+
+## Cloning
+* Run ```git clone https://github.com/prushton2/SingleFileServer```
+* Create ```.env```
+* Add ```UPLOAD_KEY={}``` which will be used for authentication for uploading information
+* Add ```GET_KEY={}``` which will be used for authentication for retrieving information
+    * Leaving either key as ```NULL``` will remove authentication for that function
+* Run ```npm start```
+* The upload endpoint is a post request at /upload, the get endpoint is a get request at the root endpoint
+* To create a valid hash: append the key to the unix time in seconds / 10 and hash with SHA256
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+const { createHash } = require("crypto");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const tokenExpiryTime = 10; //seconds
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.text());
+app.use(cookieParser());
+
+const uploadKey = process.env.UPLOAD_KEY;
+const getKey = process.env.GET_KEY;
+
+function makeHash(key) {
+    let salt = parseInt((Date.now()/1000)/tokenExpiryTime).toString();
+    // let salt = "10";
+    return createHash('sha256').update(key + salt).digest('hex').toString('base64');
+
+}
+
+function checkHash(given_key, stored_key) {
+    if(stored_key === "null") {
+        return true;
+    }
+    return given_key === makeHash(stored_key);
+}
+
+app.post("/update", async(req, res) => {
+    if(!checkHash(req.cookies.auth, uploadKey)) {
+        res.status(400);
+        res.send("Invalid Key");
+        return;
+    }
+    fs.writeFileSync("src/data.txt", req.body);
+    res.status(200);
+    res.send("File Updated");
+})
+
+
+app.get("/", async(req, res) => {
+    res.status(200);
+    res.send(fs.readFileSync("src/data.txt"));
+});
+
+app.listen(3000, () => console.log('SFS is listening on port 3000.'));
